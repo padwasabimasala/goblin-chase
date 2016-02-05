9c9fcafe5f3a08d77562db5b1e016e4890c901d2
Matthew Thorley <thorley@gmail>
2016-02-04 22:04:50 -0700

    rm favicon function

--- js/game.js
+++ js/game.js
@@ -14,19 +14,6 @@ function Game() {
 
 function init() {
 
-document.head || (document.head = document.getElementsByTagName('head')[0]);
-
-function changeFavicon(src) {
- var link = document.createElement('link'),
-     oldLink = document.getElementById('dynamic-favicon');
- link.id = 'dynamic-favicon';
- link.rel = 'shortcut icon';
- link.href = src;
- if (oldLink) {
-  document.head.removeChild(oldLink);
- }
- document.head.appendChild(link);
-}
 
 // changeFavicon("file:///Users/matthew.thorley/src/goblin-chase/images/icon.png")
 
