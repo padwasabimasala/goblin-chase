ecc45212836292e2d1757bc6920e80effe993665
Matthew Thorley <thorley@gmail>
2016-02-13 16:31:31 -0700

    actor refactor

--- js/actor.js
+++ js/actor.js
@@ -1,19 +1,72 @@
-function Actor(name, cb) {
-  this.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+/*
+ *
+ *    ActorSystem keeps an HashMap of current Actors by Id
+ *
+ *    var spawn = ActorSystem.spawn(optional actorId) // returns actorId
+ *    var receive = ActorSystem.receive(actorId, pattern, callback)
+ *    var send = ActorSystem.send(actorId || All, message)
+ *    var ANY = WebActors.ANY;
+ *
+ *    spawn registers a new actor (passing an optional id) and returns it's id
+ *
+ *    receive takes an id, a pattern, and a callback
+ *      it registers the function on that Actor
+ *      as a callback for messages matching the pattern
+ *      pattern is a regex object
+ *
+ *    send delivers a message to an Actors mailbox
+ *      messages are strings
+ *
+ *    each loop ActorSytem iterates over all the actors and calls call
+ *    Actor.call takes the oldest message from the mailbox and tries to match it
+ *    discarding it if no match
+ *
+ *
+ *
+ */
+
+function Actor(id) {
+  this.id  = id || 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
     return v.toString(16);
-  });
-  this.name = name
-  this.callback = cb
-  this.messages = new Array()
+  })
 
-  this.receive = function(message) {
-    this.messages.push(message)
+  this.message = function(message) {
+    this._message = message
     return true
   }
 
-  this.call = function() {
-    return this.callback(this.messages.shift())
+  this.receive = function(callback) {
+    this.callback = callback
   }
 
+  this.call = function() {
+    console.log(this.message)
+    return this.callback(this._message)
+
+  }
+  // this.callback = cb
+  // this.messages = new Array()
+  //
+  // this.receive = function(message) {
+  //   this.messages.push(message)
+  //   return true
+  // }
+  //
+  // this.call = function() {
+  //   return this.callback(this.messages.shift())
+  // }
+}
+
+function ActorSystem() {
+  this.actors = {}
+  this.createActor = function(name) {
+    var actor = new Actor(name)
+    this.actors[actor.uuid] = actor
+    return actor
+  }
+
+  this.send = function(actorUuid, message) {
+    this.actors[actorUuid].receive(message)
+  }
 }
--- test/actor_spec.js
+++ test/actor_spec.js
@@ -2,29 +2,77 @@
 // 2 ABC
 
 describe("Actor", function () {
-  it("has a uuid", function() {
-    var actor = new Actor("chase");
-    expect(actor.uuid).not.toBe(null)
+  it("get a default id", function() {
+    var actor = new Actor();
+    expect(actor.id).not.toBe(null)
+    expect(actor.id).not.toBe(undefined)
   })
 
-  it("returns 1 for 1", function() {
-    var actor = new Actor("chase");
-    expect(actor.name).toEqual("chase")
+  it("accepts an id", function() {
+    var actor = new Actor("theID");
+    expect(actor.id).toBe("theID")
   })
 
-  it("receives a message", function() {
-    var actor = new Actor("chase");
-    expect(actor.receive("A message")).toEqual(true)
+  it("accepts a message", function() {
+    var actor = new Actor()
+    console.log(actor.id)
+    expect(actor.message("the message")).toEqual(true)
   })
 
-  it("handles messages in the order their received", function() {
-    var actor = new Actor("chase", function(message) {
-      return message
-    })
-    actor.receive("first message")
-    actor.receive("second message")
-    expect(actor.call()).toEqual("first message")
-    expect(actor.call()).toEqual("second message")
+  it("receives a callback for a message", function() {
+    var actor = new Actor()
+    var cb = function(message) {}
+    actor.receive(cb)
+    expect(actor.callback).toEqual(cb)
   })
+
+  it("calls the callback with the message", function() {
+    var actor = new Actor()
+    var cb = function(message) { return message }
+    actor.receive(cb)
+    actor.message("the message")
+    expect(actor.call()).toEqual("the message")
+  })
+
+  // it("receives a callback for a message", function() {
+  //   var actor = new Actor()
+  //   actor.receive(function(message) {
+  //     return message
+  //   })
+  //   actor.message("the message")
+  //   expect(actor.call()).toEqual("the message")
+  // })
+  //
+
+
+
+  //
+  // it("handles messages in the order their received", function() {
+  //   var actor = new Actor("chase", function(message) {
+  //     return message
+  //   })
+  //   actor.receive("first message")
+  //   actor.receive("second message")
+  //   expect(actor.call()).toEqual("first message")
+  //   expect(actor.call()).toEqual("second message")
+  // })
+
+});
+
+describe("ActorSystem", function () {
+  // it("returns new actors", function() {
+  //   var $ = new ActorSystem()
+  //   var actor = $.createActor("phil")
+  //   expect(actor.name).toEqual("phil")
+  // })
+
+  // it("sends an actor a message", function() {
+  //   var $ = new ActorSystem()
+  //   var actor = $.createActor("phil", function(message){
+  //     return message
+  //   })
+  //   $.send(actor.uuid, "the message")
+  //   expect(actor.call()).toEqual("the message")
+  // })
 
 });
