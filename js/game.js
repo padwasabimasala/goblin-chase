38a198e17820f0534e3f5b379266d44589dfe3e7
Matthew Thorley <thorley@gmail>
2016-02-14 14:17:59 -0700

    move hero and fireball to game objects

--- js/game.js
+++ js/game.js
@@ -51,203 +51,186 @@ function GameObject(id, image_url, x, y) {
   }
 }
 function init() {
-// changeFavicon("file:///Users/matthew.thorley/src/goblin-chase/images/icon.png")
-var game = new Game(512,480)
+  // changeFavicon("file:///Users/matthew.thorley/src/goblin-chase/images/icon.png")
+  var game = new Game(512,480)
 
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-};
-heroImage.src = "images/hero.png";
+  var hero = new GameObject("hero", "images/hero.png")
+  hero.speed = 256
 
-// Fireball image
-var fireballReady = false;
-var fireballImage = new Image();
-fireballImage.onload = function() {
-  fireballReady = true;
-}
-fireballImage.src = "images/fireball.png"
+  var fireball = new GameObject("fireball", "images/fireball.png")
+  fireball.speed = 256
+  fireball.active = false
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
-};
-monsterImage.src = "images/monster.png";
+  // Monster image
+  var monsterReady = false;
+  var monsterImage = new Image();
+  monsterImage.onload = function () {
+    monsterReady = true;
+  };
+  monsterImage.src = "images/monster.png";
 
-// Monster image
-var monsterDeadReady = false;
-var monsterDeadImage = new Image();
-monsterDeadImage.onload = function () {
-	monsterDeadReady = true;
-};
-monsterDeadImage.src = "images/monster-dead.png";
+  // Monster image
+  var monsterDeadReady = false;
+  var monsterDeadImage = new Image();
+  monsterDeadImage.onload = function () {
+    monsterDeadReady = true;
+  };
+  monsterDeadImage.src = "images/monster-dead.png";
 
-var hero = {
-	speed: 256 // movement in pixels per second
-};
-var fireball = {
-  speed: 256,
-  active: false
-}
 
-fireball.timeSinceLastMove = 0;
-fireball.move = function(modifier) {
-  fireball.timeSinceLastMove += modifier
-  var fireballSpeed = 1024 * modifier
+  fireball.timeSinceLastMove = 0;
+  fireball.move = function(modifier) {
+    fireball.timeSinceLastMove += modifier
+    var fireballSpeed = 1024 * modifier
 
-  if (fireball.timeSinceLastMove > 0.5) {
-    fireball.x += fireballSpeed
-    fireball.timeSinceLastMove = 0
-  }
-}
-
-var monster = {};
-monster.reset = function() {
-  monster.alive = true
-  monster.image = monsterImage
-}
-monster.kill = function() {
-  monster.alive = false
-  monster.image = monsterDeadImage
-}
-monster.timeSinceLastMove = 0;
-monster.move = function(modifier) {
-  if (monster.alive == false) {
-    return
-  }
-  monster.timeSinceLastMove += modifier
-  var monsterSpeed = 1024 * modifier
-
-  if (monster.timeSinceLastMove > 0.5) {
-    if (Math.random() > 0.5) {
-      if (Math.random() > 0.5) {
-        monster.y += monsterSpeed
-      } else {
-        monster.y -= monsterSpeed
-      }
-    } else {
-      if (Math.random() > 0.5) {
-        monster.x += monsterSpeed
-      } else {
-        monster.x -= monsterSpeed
-      }
+    if (fireball.timeSinceLastMove > 0.5) {
+      fireball.x += fireballSpeed
+      fireball.timeSinceLastMove = 0
     }
-    monster.timeSinceLastMove = 0
-  }
-}
-var monstersShot = 0;
-
-// Handle keyboard controls
-var keysDown = {};
-
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
-}, false);
-
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
-}, false);
-
-function enforceBoundaries(character) {
-  if (character.y < 0) {
-    character.y = 0
-  }
-  if (character.y > 448) {
-    character.y = 448
-  }
-  if (character.x < 0) {
-    character.x = 0
-  }
-  if (character.x > 480) {
-    character.x =480
-  }
-}
-
-var setup = function () {
-  monster.reset()
-	hero.x = game.canvas.width / 2;
-	hero.y = game.canvas.height / 2;
-
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (game.canvas.width - 64));
-	monster.y = 32 + (Math.random() * (game.canvas.height - 64));
-};
-
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
-  if (13 in keysDown) {
-    fireball.x = hero.x + 35
-    fireball.y = hero.y
-    fireball.active = true
   }
 
-  fireball.move(modifier)
-  monster.move(modifier)
-  enforceBoundaries(hero)
-  enforceBoundaries(monster)
+  var monster = {};
+  monster.reset = function() {
+    monster.alive = true
+    monster.image = monsterImage
+  }
+  monster.kill = function() {
+    monster.alive = false
+    monster.image = monsterDeadImage
+  }
+  monster.timeSinceLastMove = 0;
+  monster.move = function(modifier) {
+    if (monster.alive == false) {
+      return
+    }
+    monster.timeSinceLastMove += modifier
+    var monsterSpeed = 1024 * modifier
 
-	// Are they touching?
-	if (
-		fireball.x <= (monster.x + 16)
-		&& monster.x <= (fireball.x + 16)
-		&& fireball.y <= (monster.y + 16)
-		&& monster.y <= (fireball.y + 16)
-	) {
-    fireball.x = -100
-    fireball.y = -100
-    fireball.active = false
-    monster.kill()
-		++monstersShot;
-    setTimeout(setup, 1500)
+    if (monster.timeSinceLastMove > 0.5) {
+      if (Math.random() > 0.5) {
+        if (Math.random() > 0.5) {
+          monster.y += monsterSpeed
+        } else {
+          monster.y -= monsterSpeed
+        }
+      } else {
+        if (Math.random() > 0.5) {
+          monster.x += monsterSpeed
+        } else {
+          monster.x -= monsterSpeed
+        }
+      }
+      monster.timeSinceLastMove = 0
+    }
+  }
+  var monstersShot = 0;
 
-	}
-};
+  // Handle keyboard controls
+  var keysDown = {};
 
+  addEventListener("keydown", function (e) {
+    keysDown[e.keyCode] = true;
+  }, false);
 
-var background = new GameObject("background", "images/background.png", 0, 0)
-// Draw everything
-var render = function () {
-	if (background.ready) { game.context.drawImage(background.image, background.x, background.y); }
-	if (heroReady) { game.context.drawImage(heroImage, hero.x, hero.y); }
-	if (monsterReady) { game.context.drawImage(monster.image, monster.x, monster.y); }
-  if (fireball.active) { game.context.drawImage(fireballImage, fireball.x, fireball.y); }
-	// Score
-	game.context.fillStyle = "rgb(250, 250, 250)";
-	game.context.font = "24px Helvetica";
-	game.context.textAlign = "left";
-	game.context.textBaseline = "top";
-	game.context.fillText("Goblins Shot: " + monstersShot, 32, 32);
-};
+  addEventListener("keyup", function (e) {
+    delete keysDown[e.keyCode];
+  }, false);
 
-// The main game loop
-requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
+  function enforceBoundaries(character) {
+    if (character.y < 0) {
+      character.y = 0
+    }
+    if (character.y > 448) {
+      character.y = 448
+    }
+    if (character.x < 0) {
+      character.x = 0
+    }
+    if (character.x > 480) {
+      character.x =480
+    }
+  }
 
-var then = Date.now();
-var main = function () {
-	var now = Date.now();
-	var delta = now - then;
-	update(delta / 1000);
-	render();
-	then = now;
-	requestAnimationFrame(main);
-};
+  var setup = function () {
+    monster.reset()
+    hero.x = game.canvas.width / 2;
+    hero.y = game.canvas.height / 2;
 
-setup();
-main();
+    // Throw the monster somewhere on the screen randomly
+    monster.x = 32 + (Math.random() * (game.canvas.width - 64));
+    monster.y = 32 + (Math.random() * (game.canvas.height - 64));
+  };
+
+  var update = function (modifier) {
+    if (38 in keysDown) { // Player holding up
+      hero.y -= hero.speed * modifier;
+    }
+    if (40 in keysDown) { // Player holding down
+      hero.y += hero.speed * modifier;
+    }
+    if (37 in keysDown) { // Player holding left
+      hero.x -= hero.speed * modifier;
+    }
+    if (39 in keysDown) { // Player holding right
+      hero.x += hero.speed * modifier;
+    }
+    if (13 in keysDown) {
+      fireball.x = hero.x + 35
+      fireball.y = hero.y
+      fireball.active = true
+    }
+
+    fireball.move(modifier)
+    monster.move(modifier)
+    enforceBoundaries(hero)
+    enforceBoundaries(monster)
+
+    // Are they touching?
+    if (
+      fireball.x <= (monster.x + 16)
+      && monster.x <= (fireball.x + 16)
+      && fireball.y <= (monster.y + 16)
+      && monster.y <= (fireball.y + 16)
+    ) {
+      fireball.x = -100
+      fireball.y = -100
+      fireball.active = false
+      monster.kill()
+      ++monstersShot;
+      setTimeout(setup, 1500)
+
+    }
+  };
+
+  var background = new GameObject("background", "images/background.png", 0, 0)
+  // Draw everything
+  var render = function () {
+    if (background.ready) { game.context.drawImage(background.image, background.x, background.y); }
+    if (hero.ready) { game.context.drawImage(hero.image, hero.x, hero.y); }
+    if (monsterReady) { game.context.drawImage(monster.image, monster.x, monster.y); }
+    if (fireball.active) { game.context.drawImage(fireball.image, fireball.x, fireball.y); }
+    // Score
+    game.context.fillStyle = "rgb(250, 250, 250)";
+    game.context.font = "24px Helvetica";
+    game.context.textAlign = "left";
+    game.context.textBaseline = "top";
+    game.context.fillText("Goblins Shot: " + monstersShot, 32, 32);
+  };
+
+  // The main game loop
+  requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
+
+  var then = Date.now();
+  var main = function () {
+    var now = Date.now();
+    var delta = now - then;
+    update(delta / 1000);
+    render();
+    then = now;
+    requestAnimationFrame(main);
+  };
+
+  setup();
+  main();
 }
